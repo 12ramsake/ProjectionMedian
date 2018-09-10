cb5594f90c3b930407f6bce10043f344883faf4e
12ramsake <32965328+12ramsake@users.noreply.github.com>
2018-09-09 20:32:55 -0400

    Update Projection Median 3 Dimensions.R

--- Projection Median 3 Dimensions.R
+++ Projection Median 3 Dimensions.R
@@ -59,20 +59,18 @@ projectionMedian3D<-function(data1){
     b<-vectors[4:6]
     return(getAngleBtwn2(a,b))
   }
+  
+  #this function is pulled from https://stackoverflow.com/questions/15162741/what-is-rs-crossproduct-function
+  #credit is to user Chip Hogg
   CrossProduct3D <- function(x, y, i=1:3) {
-    # Project inputs into 3D, since the cross product only makes sense in 3D.
+   
     To3D <- function(x) head(c(x, rep(0, 3)), 3)
     x <- To3D(x)
     y <- To3D(y)
     
-    # Indices should be treated cyclically (i.e., index 4 is "really" index 1, and
-    # so on).  Index3D() lets us do that using R's convention of 1-based (rather
-    # than 0-based) arrays.
+
     Index3D <- function(i) (i - 1) %% 3 + 1
-    
-    # The i'th component of the cross product is:
-    # (x[i + 1] * y[i + 2]) - (x[i + 2] * y[i + 1])
-    # as long as we treat the indices cyclically.
+
     return (x[Index3D(i + 1)] * y[Index3D(i + 2)] -
               x[Index3D(i + 2)] * y[Index3D(i + 1)])
   }
