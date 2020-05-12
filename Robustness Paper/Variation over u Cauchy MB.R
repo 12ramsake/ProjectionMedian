9fa257b0e4e2be21084e26a55116aa71dbe96ce2
12ramsake <32965328+12ramsake@users.noreply.github.com>
2020-05-12 13:42:29 -0400

    Add files via upload

--- Robustness Paper/Variation over u Cauchy MB.R
+++ Robustness Paper/Variation over u Cauchy MB.R
@@ -1,4 +1,4 @@
-#Cauchy differnces graphs
+#Cauchy differences graphs
 
 
 
@@ -6,14 +6,14 @@ library(ggplot2)
 library(latex2exp)
 library(ggthemes)
 #part of projection depth MB Cauchy Mariginals
-#realted to d1 and d0 in Zuo2004
+#related to d1 and d0 in Zuo2004
 solveABSquantile<-Vectorize(function(k,C,q){
   
   return(pcauchy(k+C)-pcauchy(C-k)-q)
 },vectorize.args = "k")
 
 #projection depth MB Cauchy Marginals
-getCauchyBiasPD<-function(sigma,eps,d=2){
+getCauchyBiasPD<-function(sigma,eps,d=5){
   
   au<-max(sigma,sqrt(d))
   q<-1/(2*(1-eps))
@@ -37,7 +37,7 @@ getCauchyBiasPD<-function(sigma,eps,d=2){
 getCB<-function(sigma,d,eps){
   
   #unit vs
-  m<-10000
+  m<-500000
   
   normalize<-function(x){
     return(x/sqrt(sum(x^2)))
@@ -45,50 +45,216 @@ getCB<-function(sigma,d,eps){
   x1<-replicate(d,rnorm(m))
   #d by m
   x1<-apply(x1,MARGIN=1,normalize)
-  #x1 are the unit vectors
+  #x1 are the unit vectors on upper hemisphere
   
-  #q1 quantile, 1/2(1-e) quantile of projection onto u
+  x1[,x1[d,]<0]=-x1[,x1[d,]<0]
+  
+  
+  #a(u)u
   getFinv<-function(u){
+    #a(u)
     scale<-sum(abs(u[1:(d-1)]))+abs(u[d])*sigma
+    
     #print(scale)
-    return(qcauchy(1/(2*(1-eps)),scale=scale))
+    return(scale*u)
   }
   
-  int<-mean(apply(x1,2,getFinv))
-  return(int*d)
+  #integral
+  int_vals=apply(x1,2,getFinv); dim(int_vals)
+  int_vals=rowMeans(int_vals)
+  int_vals=sqrt(sum(int_vals^2))
+  
+  return(int_vals*d*qcauchy(1/(2*(1-eps))))
 }
 
 #MB for Cauchy marginals, as per Chen 2002
-getCBHM<-Vectorize(function(sigma,eps,d=2){
+getCBHM<-Vectorize(function(sigma,eps,d=5){
   return(max(sigma,sqrt(d))*tan(pi*eps/(1-eps)))
 },vectorize.args = 'd')
 
 
 #epsilons
-epss<-seq(.05,.45,length.out = 100)
+e_length=100
+epss<-seq(.01,.45,length.out = e_length)
 
 sigmas<-c(1,2,5,8,10)
 
 #projection depth mb
-biasesPD<-matrix(0,nrow=length(sigmas),ncol=100)
+biasesPD<-matrix(0,nrow=length(sigmas),ncol=e_length)
 for(i in 1:length(sigmas))
-  for(j in 1:100)
+  for(j in 1:e_length)
     biasesPD[i,j]<-getCauchyBiasPD(sigmas[i],epss[j],d=2)
 
 biasesPD
 
 #halfspace median
-biasesHM<-matrix(0,nrow=length(sigmas),ncol=100)
+biasesHM<-matrix(0,nrow=length(sigmas),ncol=e_length)
 for(i in 1:length(sigmas))
-  for(j in 1:100)
+  for(j in 1:e_length)
+    biasesHM[i,j]<-getCBHM(sigmas[i],epss[j],d=2)
+# biasesHM
+
+#projection median
+biasesPM<-matrix(0,nrow=length(sigmas),ncol=e_length)
+for(i in 1:length(sigmas))
+  for(j in 1:e_length)
+    biasesPM[i,j]<-getCB(sigmas[i],d=2,epss[j])
+biasesPM
+
+
+
+
+#make data frame for ggplot and plot
+
+Epsilon<-sort(rep(epss,5))
+Scale=rep(sigmas,length(epss))
+Bias<-c(biasesPM-biasesPD)
+
+dat<-data.frame(cbind(Epsilon,Scale,Bias))
+dat$Scale=as.factor(dat$Scale)
+ggplot(dat, aes(x=Epsilon,y=Bias,
+                color=Scale,group=Scale))+
+    #stat_smooth(lwd=1.5,se=FALSE)+
+  geom_line(lwd=1.5)+
+  ylab(TeX('$\\mathbf{B}(PMD,\\epsilon,\\mathit{F})-\\mathbf{B}(PM_{zuo},\\epsilon,\\mathit{F})$'))+
+  xlab(TeX('$\\epsilon$'))+
+# coord_cartesian(ylim=c(-160,-1)) +
+  scale_color_economist()+
+  theme_economist()+
+  #scale_color_brewer(type = 'seq', palette ='PuRd', direction = 1)+
+  theme(plot.title = element_text(size = 40, face = "bold"),
+        axis.title.x = element_text(size=20),
+        axis.text.x = element_text(size=20),
+        axis.text.y = element_text(size=20),
+        axis.title.y = element_text(size=20),
+        legend.text=element_text(size=20),
+        legend.title=element_text(size=20))#+
+#  ggtitle("Bias Difference, PMZ")
+
+
+#epsilon only goes up to .33 for HM
+epss<-seq(.05,.3,length.out = e_length)
+
+biasesHM<-matrix(0,nrow=length(sigmas),ncol=e_length)
+for(i in 1:length(sigmas))
+  for(j in 1:e_length)
     biasesHM[i,j]<-getCBHM(sigmas[i],epss[j],d=2)
 biasesHM
 
-#projection median
-biasesPM<-matrix(0,nrow=length(sigmas),ncol=100)
+
+biasesPM<-matrix(0,nrow=length(sigmas),ncol=e_length)
 for(i in 1:length(sigmas))
-  for(j in 1:100)
+  for(j in 1:e_length)
     biasesPM[i,j]<-getCB(sigmas[i],d=2,epss[j])
+biasesPM
+
+
+
+
+
+Epsilon<-sort(rep(epss,5))
+Scale=rep(sigmas,length(epss))
+Bias<-c(biasesPM-biasesHM)
+#8x9
+dat<-data.frame(cbind(Epsilon,Scale,Bias))
+dat$Scale=as.factor(dat$Scale)
+ggplot(dat, aes(x=Epsilon,y=Bias,
+                color=Scale,group=Scale))+
+  ylab(TeX('$\\mathbf{B}(PMD,\\epsilon,\\mathit{F})-\\mathbf{B}(HM,\\epsilon,\\mathit{F})$'))+
+  #stat_smooth(lwd=1.5,se=FALSE)+
+  geom_line(lwd=1.5)+
+  xlab(TeX('$\\epsilon$'))+
+  #  coord_cartesian(ylim=c(-1,4)) +
+  scale_color_economist()+
+  theme_economist()+
+  #scale_color_brewer(type = 'seq', palette ='PuRd', direction = 1)+
+  theme(plot.title = element_text(size = 40, face = "bold"),
+        axis.title.x = element_text(size=20),
+        axis.text.x = element_text(size=20),
+        axis.text.y = element_text(size=20),
+        axis.title.y = element_text(size=20),
+        legend.text=element_text(size=20),
+        legend.title=element_text(size=20))#+
+ # ggtitle("Bias Difference, HM")
+
+
+#variation over u
+
+a<-sigmas
+grid<-seq(0,pi,length.out = 200)
+f2<-Vectorize(function(val,x=grid){sum(abs(matrix(c(cos(x),sin(x)),nrow=1))*c(1,val))},vectorize.args = 'x')
+
+
+tmp<-f2(1,grid)
+mat<-cbind(grid,tmp,rep(1,length(grid)))
+colnames(mat)<-c("Epsilon","Bias","Scale")
+row.names(mat)<-NULL
+dat2<-data.frame(mat)
+
+
+names(dat2)<-c("Epsilon","Bias","Scale")
+for(i in 1:length(a)){
+  tmp<-f2(a[i],grid)
+  mat<-cbind(grid,tmp,rep(a[i],length(grid)))
+  colnames(mat)<-c("Epsilon","Bias","Scale")
+  row.names(mat)<-NULL
+  #  inff<-c(0.5,1000,a[i])
+  dat2<-rbind(dat2,mat)
+}
+
+
+
+
+dat2$Scale=as.factor(dat2$Scale)
+ggplot(dat2, aes(x=Epsilon,y=Bias,
+                 color=Scale,group=Scale))+
+  ylab(TeX('$a(u)$'))+
+  #stat_smooth(lwd=1.5,se=FALSE)+
+  geom_line(lwd=1.5)+
+  #coord_cartesian(ylim=c(-20,0.15)) +
+  theme_economist()+ 
+  #  theme(legend.position = c(2.5, 20))+
+  xlab("Spherical Angle")+
+  
+  scale_color_economist()+
+  
+  #scale_color_brewer(type = 'seq', palette ='PuRd', direction = 1)+
+  theme(plot.title = element_text(size = 40, face = "bold"),
+        axis.title.x = element_text(size=20),
+        axis.text.x = element_text(size=20),
+        axis.text.y = element_text(size=20),
+        axis.title.y = element_text(size=20),
+        legend.text=element_text(size=20),
+        legend.title=element_text(size=20))
+#+
+  #ggtitle("Variation over u")
+
+
+
+
+################d=5
+
+epss<-seq(.01,.45,length.out = e_length)
+#projection depth mb
+biasesPD<-matrix(0,nrow=length(sigmas),ncol=e_length)
+for(i in 1:length(sigmas))
+  for(j in 1:e_length)
+    biasesPD[i,j]<-getCauchyBiasPD(sigmas[i],epss[j],d=5)
+
+biasesPD
+
+#halfspace median
+biasesHM<-matrix(0,nrow=length(sigmas),ncol=e_length)
+for(i in 1:length(sigmas))
+  for(j in 1:e_length)
+    biasesHM[i,j]<-getCBHM(sigmas[i],epss[j],d=5)
+biasesHM
+
+#projection median
+biasesPM<-matrix(0,nrow=length(sigmas),ncol=e_length)
+for(i in 1:length(sigmas))
+  for(j in 1:e_length)
+    biasesPM[i,j]<-getCB(sigmas[i],d=5,epss[j])
 biasesPM
 
 
@@ -106,7 +272,7 @@ ggplot(dat, aes(x=Epsilon,y=Bias,
                 color=Scale,group=Scale))+
   #stat_smooth(lwd=1.5,se=FALSE)+
   geom_line(lwd=1.5)+
-  ylab(TeX('$\\mathbf{B}(\\mathit{M},\\epsilon,\\mathit{F})-\\mathbf{B}(\\mathit{PMZ},\\epsilon,\\mathit{F})$'))+
+  ylab(TeX('$\\mathbf{B}(PMD,\\epsilon,\\mathit{F})-\\mathbf{B}(PM_{zuo},\\epsilon,\\mathit{F})$'))+
   xlab(TeX('$\\epsilon$'))+
   # coord_cartesian(ylim=c(-160,-1)) +
   scale_color_economist()+
@@ -123,19 +289,19 @@ ggplot(dat, aes(x=Epsilon,y=Bias,
 
 
 #epsilon only goes up to .33 for HM
-epss<-seq(.05,.3,length.out = 100)
+epss<-seq(.05,.3,length.out = e_length)
 
-biasesHM<-matrix(0,nrow=length(sigmas),ncol=100)
+biasesHM<-matrix(0,nrow=length(sigmas),ncol=e_length)
 for(i in 1:length(sigmas))
-  for(j in 1:100)
-    biasesHM[i,j]<-getCBHM(sigmas[i],epss[j],d=2)
+  for(j in 1:e_length)
+    biasesHM[i,j]<-getCBHM(sigmas[i],epss[j],d=5)
 biasesHM
 
 
-biasesPM<-matrix(0,nrow=length(sigmas),ncol=100)
+biasesPM<-matrix(0,nrow=length(sigmas),ncol=e_length)
 for(i in 1:length(sigmas))
-  for(j in 1:100)
-    biasesPM[i,j]<-getCB(sigmas[i],2,epss[j])
+  for(j in 1:e_length)
+    biasesPM[i,j]<-getCB(sigmas[i],d=5,epss[j])
 biasesPM
 
 
@@ -150,7 +316,7 @@ dat<-data.frame(cbind(Epsilon,Scale,Bias))
 dat$Scale=as.factor(dat$Scale)
 ggplot(dat, aes(x=Epsilon,y=Bias,
                 color=Scale,group=Scale))+
-  ylab(TeX('$\\mathbf{B}(\\mathit{M},\\epsilon,\\mathit{F})-\\mathbf{B}(\\mathit{HM},\\epsilon,\\mathit{F})$'))+
+  ylab(TeX('$\\mathbf{B}(PMD,\\epsilon,\\mathit{F})-\\mathbf{B}(HM,\\epsilon,\\mathit{F})$'))+
   #stat_smooth(lwd=1.5,se=FALSE)+
   geom_line(lwd=1.5)+
   xlab(TeX('$\\epsilon$'))+
@@ -220,8 +386,156 @@ ggplot(dat2, aes(x=Epsilon,y=Bias,
 #ggtitle("Variation over u")
 
 
+################d=10
+epss<-seq(.01,.45,length.out = e_length)
+
+#projection depth mb
+biasesPD<-matrix(0,nrow=length(sigmas),ncol=e_length)
+for(i in 1:length(sigmas))
+  for(j in 1:e_length)
+    biasesPD[i,j]<-getCauchyBiasPD(sigmas[i],epss[j],d=10)
+
+biasesPD
+
+#halfspace median
+biasesHM<-matrix(0,nrow=length(sigmas),ncol=e_length)
+for(i in 1:length(sigmas))
+  for(j in 1:e_length)
+    biasesHM[i,j]<-getCBHM(sigmas[i],epss[j],d=10)
+biasesHM
+
+#projection median
+biasesPM<-matrix(0,nrow=length(sigmas),ncol=e_length)
+for(i in 1:length(sigmas))
+  for(j in 1:e_length)
+    biasesPM[i,j]<-getCB(sigmas[i],d=10,epss[j])
+biasesPM
 
 
 
 
+#make data frame for ggplot and plot
+
+Epsilon<-sort(rep(epss,5))
+Scale=rep(sigmas,length(epss))
+Bias<-c(biasesPM-biasesPD)
+
+dat<-data.frame(cbind(Epsilon,Scale,Bias))
+dat$Scale=as.factor(dat$Scale)
+ggplot(dat, aes(x=Epsilon,y=Bias,
+                color=Scale,group=Scale))+
+  #stat_smooth(lwd=1.5,se=FALSE)+
+  geom_line(lwd=1.5)+
+  ylab(TeX('$\\mathbf{B}(PMD,\\epsilon,\\mathit{F})-\\mathbf{B}(PM_{zuo},\\epsilon,\\mathit{F})$'))+
+  xlab(TeX('$\\epsilon$'))+
+  # coord_cartesian(ylim=c(-160,-1)) +
+  scale_color_economist()+
+  theme_economist()+
+  #scale_color_brewer(type = 'seq', palette ='PuRd', direction = 1)+
+  theme(plot.title = element_text(size = 40, face = "bold"),
+        axis.title.x = element_text(size=20),
+        axis.text.x = element_text(size=20),
+        axis.text.y = element_text(size=20),
+        axis.title.y = element_text(size=20),
+        legend.text=element_text(size=20),
+        legend.title=element_text(size=20))#+
+#  ggtitle("Bias Difference, PMZ")
+
+
+#epsilon only goes up to .33 for HM
+epss<-seq(.05,.3,length.out = e_length)
+
+biasesHM<-matrix(0,nrow=length(sigmas),ncol=e_length)
+for(i in 1:length(sigmas))
+  for(j in 1:e_length)
+    biasesHM[i,j]<-getCBHM(sigmas[i],epss[j],d=10)
+biasesHM
+
+
+biasesPM<-matrix(0,nrow=length(sigmas),ncol=e_length)
+for(i in 1:length(sigmas))
+  for(j in 1:e_length)
+    biasesPM[i,j]<-getCB(sigmas[i],d=10,epss[j])
+biasesPM
+
+
+
+
+
+Epsilon<-sort(rep(epss,5))
+Scale=rep(sigmas,length(epss))
+Bias<-c(biasesPM-biasesHM)
+#8x9
+dat<-data.frame(cbind(Epsilon,Scale,Bias))
+dat$Scale=as.factor(dat$Scale)
+ggplot(dat, aes(x=Epsilon,y=Bias,
+                color=Scale,group=Scale))+
+  ylab(TeX('$\\mathbf{B}(PMD,\\epsilon,\\mathit{F})-\\mathbf{B}(HM,\\epsilon,\\mathit{F})$'))+
+  #stat_smooth(lwd=1.5,se=FALSE)+
+  geom_line(lwd=1.5)+
+  xlab(TeX('$\\epsilon$'))+
+  #  coord_cartesian(ylim=c(-1,4)) +
+  scale_color_economist()+
+  theme_economist()+
+  #scale_color_brewer(type = 'seq', palette ='PuRd', direction = 1)+
+  theme(plot.title = element_text(size = 40, face = "bold"),
+        axis.title.x = element_text(size=20),
+        axis.text.x = element_text(size=20),
+        axis.text.y = element_text(size=20),
+        axis.title.y = element_text(size=20),
+        legend.text=element_text(size=20),
+        legend.title=element_text(size=20))#+
+# ggtitle("Bias Difference, HM")
+
+
+#variation over u
+
+a<-sigmas
+grid<-seq(0,pi,length.out = 200)
+f2<-Vectorize(function(val,x=grid){sum(abs(matrix(c(cos(x),sin(x)),nrow=1))*c(1,val))},vectorize.args = 'x')
+
+
+tmp<-f2(1,grid)
+mat<-cbind(grid,tmp,rep(1,length(grid)))
+colnames(mat)<-c("Epsilon","Bias","Scale")
+row.names(mat)<-NULL
+dat2<-data.frame(mat)
+
+
+names(dat2)<-c("Epsilon","Bias","Scale")
+for(i in 1:length(a)){
+  tmp<-f2(a[i],grid)
+  mat<-cbind(grid,tmp,rep(a[i],length(grid)))
+  colnames(mat)<-c("Epsilon","Bias","Scale")
+  row.names(mat)<-NULL
+  #  inff<-c(0.5,1000,a[i])
+  dat2<-rbind(dat2,mat)
+}
+
+
+
+
+dat2$Scale=as.factor(dat2$Scale)
+ggplot(dat2, aes(x=Epsilon,y=Bias,
+                 color=Scale,group=Scale))+
+  ylab(TeX('$a(u)$'))+
+  #stat_smooth(lwd=1.5,se=FALSE)+
+  geom_line(lwd=1.5)+
+  #coord_cartesian(ylim=c(-20,0.15)) +
+  theme_economist()+ 
+  #  theme(legend.position = c(2.5, 20))+
+  xlab("Spherical Angle")+
+  
+  scale_color_economist()+
+  
+  #scale_color_brewer(type = 'seq', palette ='PuRd', direction = 1)+
+  theme(plot.title = element_text(size = 40, face = "bold"),
+        axis.title.x = element_text(size=20),
+        axis.text.x = element_text(size=20),
+        axis.text.y = element_text(size=20),
+        axis.title.y = element_text(size=20),
+        legend.text=element_text(size=20),
+        legend.title=element_text(size=20))
+#+
+#ggtitle("Variation over u")
 
